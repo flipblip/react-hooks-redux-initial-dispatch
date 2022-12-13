6d355bfdcf6f0d592cc85dc1e0c4b7c06c5f8c5a
fliblip <wickynjy@gmail.com>
2022-12-13 15:56:31 +0300

    Done

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
-let state = { count: 0 };
+// Decalre but don not assign state || State will start off undefined
+let state;
 
-function changeState(state, action) {
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -18,3 +19,10 @@ function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+
+// Dispatch @@INIT, which calls dispatch(), dispatch will pass through our initial action
+// Dispatch then calls changeState() reducer, passing two local variables: state and action
+// Basically, we're calling changeState(undefined, { type: "@@INIT"})
+// When changeState() reducer returns, dispatch assigns it a return value, updating the4 initial sate to { count: 0 }. 
+// On the next line, render() displays 0 in our HTML
+dispatch({ type: "@@INIT"})
